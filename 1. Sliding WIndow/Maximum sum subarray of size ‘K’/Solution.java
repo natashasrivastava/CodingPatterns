ff88ff6f960b10980a0249e23266b27124af035a
Natasha Srivastava <66521997+natashasrivastava@users.noreply.github.com>
2022-10-16 00:27:49 +0530

    Update and rename maxSum.java to Solution.java

--- 1. Sliding WIndow/Maximum sum subarray of size ‘K’/Solution.java
+++ 1. Sliding WIndow/Maximum sum subarray of size ‘K’/Solution.java
@@ -1,7 +1,7 @@
 //Given an array of integers Arr of size N and a number K. Return the maximum sum of a subarray of size K. 
+
 //[Try solving this on Geeks for Geeks](https://practice.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1)
 
-//Solution in Java
 
 class Solution{
     static long maximumSumSubarray(int K, ArrayList<Integer> arr,int N){
