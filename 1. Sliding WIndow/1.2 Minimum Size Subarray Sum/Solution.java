3dd60b61a6d61ab827d67dbdb6e0baceae3ef33f
Natasha Srivastava <66521997+natashasrivastava@users.noreply.github.com>
2022-10-16 02:44:48 +0530

    Create Solution.java

---
+++ 1. Sliding WIndow/1.2 Minimum Size Subarray Sum/Solution.java
@@ -0,0 +1,28 @@
+/* Given an array of positive integers nums and a positive integer target, return the minimal length of a contiguous subarray 
+    [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is greater than or equal to target. 
+    If there is no such subarray, return 0 instead.
+*/
+
+// Try this question on Leetcode https://leetcode.com/problems/minimum-size-subarray-sum/solutions/
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int windowStart = 0;
+        int windowEnd = 0;
+        int windowSize = Integer.MAX_VALUE;
+        int windowSum = 0;
+        
+        for(windowEnd=0;windowEnd<nums.length;windowEnd++){
+            windowSum += nums[windowEnd];
+            while(windowSum>=target){
+                windowSize = Math.min(windowSize, windowEnd - windowStart + 1);
+                windowSum -= nums[windowStart];
+                windowStart++;
+            }
+        }
+        if(windowSize==Integer.MAX_VALUE){
+            return 0;
+        }
+        return windowSize;
+    }
+}
