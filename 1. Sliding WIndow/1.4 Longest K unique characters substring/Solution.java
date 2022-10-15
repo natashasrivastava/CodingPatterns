266676cdf2ada82a8a10c3b3476600a6cff49e57
Natasha Srivastava <66521997+natashasrivastava@users.noreply.github.com>
2022-10-16 03:27:46 +0530

    Create Solution.java

---
+++ 1. Sliding WIndow/1.4 Longest K unique characters substring/Solution.java
@@ -0,0 +1,32 @@
+/*  Given a string you need to print the size of the longest possible substring that has exactly K unique characters. 
+    If there is no possible substring then print -1.
+*/
+
+class Solution {
+    public int longestkSubstr(String s, int k) {
+        int windowStart = 0;
+        int windowSize = 0;
+        int uniqueCount = 0;
+        int[] alphabets = new int[27];
+        for(int windowEnd = 0; windowEnd < s.length() ; windowEnd++){
+            alphabets[s.charAt(windowEnd) - 'a']++;
+            if(alphabets[s.charAt(windowEnd) - 'a']==1){
+                uniqueCount++;
+            }
+            if(uniqueCount==k){
+                windowSize = Math.max(windowSize, windowEnd - windowStart + 1);
+            }
+            while(uniqueCount>k && windowStart < s.length()){
+                alphabets[s.charAt(windowStart) - 'a']--;
+                if(alphabets[s.charAt(windowStart) - 'a']==0){
+                    uniqueCount--;
+                }
+                windowStart++;
+            }
+        }
+        if(windowSize==0){
+            return -1;
+        }
+        return windowSize;
+    }
+}
