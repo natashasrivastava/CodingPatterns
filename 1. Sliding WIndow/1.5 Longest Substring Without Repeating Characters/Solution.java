c9539086fe15ad113c41e9a387995355d41a441f
Natasha Srivastava <66521997+natashasrivastava@users.noreply.github.com>
2022-10-23 10:50:07 +0530

    Create Solution.java

---
+++ 1. Sliding WIndow/1.5 Longest Substring Without Repeating Characters/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int windowSize = 0;
+        int windowStart = 0;
+        int notUniqueCount = 0;
+        HashMap<Character, Integer> alphabets = new HashMap<Character, Integer> ();
+        for(int windowEnd = 0; windowEnd < s.length(); windowEnd++){
+            while(alphabets.containsKey(s.charAt(windowEnd)) && windowStart<s.length()){
+                alphabets.put(s.charAt(windowStart), alphabets.getOrDefault(s.charAt(windowStart),0)-1);
+                if(alphabets.get(s.charAt(windowStart))<=0){
+                    alphabets.remove(s.charAt(windowStart));
+                }
+                windowStart++;
+            }
+            alphabets.put(s.charAt(windowEnd), 1);
+            windowSize = Math.max(windowSize, windowEnd - windowStart + 1);
+        }
+        return windowSize;
+    }
+ }
