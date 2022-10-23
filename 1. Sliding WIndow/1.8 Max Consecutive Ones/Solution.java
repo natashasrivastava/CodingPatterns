2ae962cb5597e69859a2be382d90cb8c08600d3c
Natasha Srivastava <66521997+natashasrivastava@users.noreply.github.com>
2022-10-23 21:37:22 +0530

    Create Solution.java

---
+++ 1. Sliding WIndow/1.8 Max Consecutive Ones/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int windowStart = 0;
+        int windowSize = 0;
+        int zeroesCount = 0;
+        for(int windowEnd = 0; windowEnd< nums.length; windowEnd++){
+            if(nums[windowEnd]==0){
+                zeroesCount++;
+            }
+            while(windowStart<nums.length && zeroesCount>k){
+                if(nums[windowStart]==0){
+                    zeroesCount--;
+                }
+                windowStart++;
+            }
+            windowSize = Math.max(windowSize, windowEnd - windowStart + 1);
+        }
+        return windowSize;
+    }
+}
