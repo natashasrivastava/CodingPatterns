df19dc1018ccf2b84bf81d93be9d1eb35604ef2b
Natasha Srivastava <66521997+natashasrivastava@users.noreply.github.com>
2022-10-24 13:40:05 +0530

    Update Solution.java

--- 1. Sliding WIndow/1.4 Longest K unique characters substring/Solution.java
+++ 1. Sliding WIndow/1.4 Longest K unique characters substring/Solution.java
@@ -1,7 +1,12 @@
-/*  Given a string you need to print the size of the longest possible substring that has exactly K unique characters. 
-    If there is no possible substring then print -1.
-    
-    Try solving this question on GFG https://practice.geeksforgeeks.org/problems/longest-k-unique-characters-substring0853/1
+/*  
+1. First, we will insert characters from the beginning of the string until we have ‘K’ distinct characters in the HashMap.
+2. These characters will constitute our sliding window. We are asked to find the longest such window having no more than ‘K’ distinct characters. 
+    We will remember the length of this window as the longest window so far.
+3. After this, we will keep adding one character in the sliding window (i.e. slide the window ahead), in a stepwise fashion.
+4. In each step, we will try to shrink the window from the beginning if the count of distinct characters in the HashMap is larger than ‘K’. 
+    We will shrink the window until we have no more than ‘K’ distinct characters in the HashMap. This is needed as we intend to find the longest window.
+5. While shrinking, we’ll decrement the frequency of the character going out of the window and remove it from the HashMap if its frequency becomes zero.
+6. At the end of each step, we’ll check if the current window length is the longest so far, and if so, remember its length.
 */
 
 class Solution {
