5a20e91c7e7023e757cf6761799b77c2cedd61dd
Natasha Srivastava <66521997+natashasrivastava@users.noreply.github.com>
2022-11-01 22:57:02 +0530

    Update Solution.java

--- 2. Two Pointers/2.1 Pair with Target Sum/Solution.java
+++ 2. Two Pointers/2.1 Pair with Target Sum/Solution.java
@@ -4,15 +4,23 @@ class Solution {
         int i = 0, j = n-1;
         int[] answer = new int[2];
         while(i<j){
+            // Take 2 pointers as i and j, i starts from starting and j starts from end of array
+            // Calculate sum of elements at these pointers in array
             int sum = numbers[i]+numbers[j];
+            
+            // When target is found as equal to sum, add it to array and return
             if(sum==target){
                 answer[0] = i+1;
                 answer[1] = j+1;
                 return answer;
             }
+            
+            // If the sum is less than target, increment i as we need a greater number
             else if(sum < target){
                 i++;
             }
+            
+            // If sum is greater than target, decrement j as we need a smaller number
             else{
                 j--;
             }
