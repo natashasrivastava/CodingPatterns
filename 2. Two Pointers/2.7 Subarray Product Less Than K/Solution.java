e98125cca07b144ac71571641dbb1dd8a1b09414
Natasha Srivastava <66521997+natashasrivastava@users.noreply.github.com>
2023-04-23 14:35:07 +0530

    Update Solution.java

--- 2. Two Pointers/2.7 Subarray Product Less Than K/Solution.java
+++ 2. Two Pointers/2.7 Subarray Product Less Than K/Solution.java
@@ -3,7 +3,7 @@ class Solution {
         if (k <= 1) return 0;
       
         int prod = 1, ans = 0, left = 0;
-      // Finding a sliding window that has product less than target.
+      // Expaning a sliding window to max size till the product is less than target.
         for (int right = 0; right < nums.length; right++) {
             prod *= nums[right];
           // Whenever product becomes greater than target, squeeze the window from left until prod<k
