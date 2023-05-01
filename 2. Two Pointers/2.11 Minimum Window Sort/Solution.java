37450120dcc9182022173a00a489f03b2d4c40a0
Natasha Srivastava <66521997+natashasrivastava@users.noreply.github.com>
2023-05-01 10:44:49 +0530

    Update Solution.java

--- 2. Two Pointers/2.11 Minimum Window Sort/Solution.java
+++ 2. Two Pointers/2.11 Minimum Window Sort/Solution.java
@@ -1,3 +1,12 @@
+// Once an array is sorted (in ascending order), the smallest number is at the beginning and the largest number is at the end of the array. 
+// So if we start from the beginning of the array to find the first element which is out of sorting order i.e., which is smaller than its previous element, and similarly from the end of array to find the first element which is bigger than its previous element
+// sorting the subarray between these two numbers result in the whole array being sorted
+
+//1. From the beginning and end of the array, find the first elements that are out of the sorting order. The two elements will be our candidate subarray.
+//2.Find the maximum and minimum of this subarray.
+//3. Extend the subarray from beginning to include any number which is bigger than the minimum of the subarray.
+//4. Similarly, extend the subarray from the end to include any number which is smaller than the maximum of the subarray.
+
 public class Solution {
     public int findUnsortedSubarray(int[] nums) {
         int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
